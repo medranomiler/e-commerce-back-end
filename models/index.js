a995ec701bb499985fc3238d326bc070b2d2e673
Medranomiler1 <medranomiler@gmail.com>
2023-02-07 22:00:02 -0600

    added models and first get route to finad all categories

--- models/index.js
+++ models/index.js
@@ -5,12 +5,27 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: 'product_id',
+});
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'product_id',
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  as: 'tags',
+  foreignKey: 'product_id'
+})
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  as: 'products',
+  foreignKey: 'product_id'
+})
 
 module.exports = {
   Product,
