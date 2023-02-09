6062d13303fcd37756375865f248f50d070985da
Medranomiler1 <medranomiler@gmail.com>
2023-02-08 21:27:49 -0600

    added more routes and corrected index.js models

--- models/index.js
+++ models/index.js
@@ -6,11 +6,13 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 Product.belongsTo(Category, {
-  foreignKey: 'product_id',
+  foreignKey: 'category_id',
 });
+
 // Categories have many Products
 Category.hasMany(Product, {
-  foreignKey: 'product_id',
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE'
 });
 
 // Products belongToMany Tags (through ProductTag)
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,19 +4,43 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
+  try{
+    const productData = await Product.findAll(req.params.id, {
   // be sure to include its associated Category and Tag data
+   include: [{ model: Category}, {model: Tag}]
+    })
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
+
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
+
+  try {
+    const productData = await Product.findByPk(req.params.id, {
   // be sure to include its associated Category and Tag data
+    });
+
+    if (!productData) {
+      res.status(404).json({ message: 'No products found with that id!' });
+      return;
+    }
+
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
+
 // create new product
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   /* req.body should look like this...
     {
       product_name: "Basketball",
@@ -25,6 +49,7 @@ router.post('/', (req, res) => {
       tagIds: [1, 2, 3, 4]
     }
   */
+
   Product.create(req.body)
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
